d96be47289ee1299fc595dcef060b7380f12d9c8
jiseong1484 <tony6356@naver.com>
2025-07-21 12:48:16 +0900

    [feat] activated 인 가계만 리스트에 표시

--- frontend/app/(protected)/home/page.tsx
+++ frontend/app/(protected)/home/page.tsx
@@ -76,6 +76,7 @@ export default function HomePage() {
       const { data, error } = await supabase
         .from("stores")
         .select("*, discounts(*, store_menus(*))")
+        .eq('activated', true) //activated == true 인 가게만 home page에 표시
 
       if (error) {
         console.error("Error fetching stores from DB:", error)
